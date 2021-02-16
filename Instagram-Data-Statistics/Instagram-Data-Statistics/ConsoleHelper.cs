f28a799ca2b241af4146917328cf937f56349b59
HiMyNameIsGarch <gabrielhanu700@yahoo.com>
2021-02-16 22:35:58 +0200

    add more options for user and a console helper

---
+++ Instagram-Data-Statistics/Instagram-Data-Statistics/ConsoleHelper.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Instagram_Data_Statistics
+{
+    public static class ConsoleHelper 
+    {
+        public static string GetValue(string text)
+        {
+            while (true)
+            {
+                Console.WriteLine(text);
+                string value = Console.ReadLine();
+                if (!string.IsNullOrEmpty(value)) return value;
+                ClearLines(2);
+            }
+        }
+        public static int GetNum(string text, int maxValue = int.MaxValue)
+        {
+            Console.WriteLine(text);
+            while (true)
+            {
+                if (int.TryParse(Console.ReadLine(), out int numberOfLikes) && (numberOfLikes > 0 && numberOfLikes <= maxValue))
+                {
+                    return numberOfLikes;
+                }
+                ClearLines();
+            }
+        }
+        public static void ShowList(List<KeyValuePair<string, int>> list)
+        {
+            foreach (var item in list)
+            {
+                if (item.Value == 1)
+                    Console.WriteLine("You liked post from {0} one time", item.Key);
+                else
+                    Console.WriteLine("You liked post from {0} {1} times", item.Key, item.Value);
+            }
+        }
+        public static ConsoleKey GetChoice(string QAndA, ConsoleKey[] bounds)
+        {
+            while (true)
+            {
+                Console.WriteLine(QAndA);
+                var keyFromUser = Console.ReadKey(true).Key;
+                foreach (var key in bounds)
+                {
+                    if (key == keyFromUser) return key;
+                }
+                ClearLines(bounds.Length + 1);
+            }
+        }
+        public static void WaitAndClearLines(int linesToClear, int timeToWait, string msgToShow)
+        {
+            Console.WriteLine(msgToShow);
+            Thread.Sleep(timeToWait);
+            ClearLines(linesToClear);
+        }
+        public static void ClearLines(int lines = 1)
+        {
+            if (lines > 0)
+            {
+                Console.SetCursorPosition(0, Console.CursorTop - lines);
+                for (int i = 0; i < lines; i++)
+                {
+                    Console.Write(new string(' ', Console.BufferWidth));
+                }
+                Console.SetCursorPosition(0, Console.CursorTop - lines);
+            }
+        }
+    }
+}
---
+++ Instagram-Data-Statistics/Instagram-Data-Statistics/Enums/DataType.cs
@@ -0,0 +1,16 @@
+﻿namespace Instagram_Data_Statistics.Enums
+{
+    public enum DataType
+    {
+        Likes,//comment and media
+        Account_History,//login history, registration info
+        Comments,
+        Media,//includes all the files that you send(stories, profile,photos,videos, direct)
+        Connections,//all blocked users, follow request send, permanent follow request, followers, following, hastags, dimissed suggested user
+        Messages,
+        Saved,//collections and media
+        Searched_Content,//all accounts that you searched
+        Seen_Content,//all the posts that you saw
+        Stories//polls,emoji slider, questions, countdowns,quizzes
+    }
+}
